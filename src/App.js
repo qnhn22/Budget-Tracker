d733c1736418d84ccb46a1221bbe3e60ee50d446
quannn22 <nguyenqn@dickinson.edu>
2022-12-27 20:56:48 -0500

    basic build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import { Stack, Button } from 'react-bootstrap';
+import BudgetCard from './BudgetCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className='mt-3'>
+      <Stack direction='horizontal' gap={2} className="mb-4">
+        <h1 className='me-auto'>Budgetify</h1>
+        <Button variant='primary'>Add Budget</Button>
+        <Button variant='secondary'>Add Expense</Button>
+      </Stack>
+      <Container fluid>
+        <BudgetCard name={"Housing"} amount={2500} availability={2000} />
+        <BudgetCard name={"Food"} amount={800} availability={1200} />
+        <BudgetCard />
+        <BudgetCard />
+      </Container>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/BudgetCard.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card';
+import { currencyFormatter } from './utils';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import { Stack, Button } from 'react-bootstrap';
+
+function BudgetCard({ name, amount, availability }) {
+    const classNames = ""
+    if (amount > availability) {
+        classNames = "bg-danget"
+    } else {
+        classNames.push("bg-light")
+    }
+    console.log(amount)
+    console.log(availability)
+
+    return (
+        <Card className="bg-danger">
+            <Card.Body >
+                <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal mb-3'>
+                    <div className='me-2'>{name}</div>
+                    <div className='d-flex align-items-baseline'>{currencyFormatter.format(amount)} /
+                        <span className='text-muted fs-6 ms-1'>{availability}</span>
+                    </div>
+                </Card.Title>
+                <ProgressBar className='rounded-pill' variant={getProBarVar(amount, availability)}
+                    min={0} max={availability} now={amount}></ProgressBar>
+                <Stack direction='horizontal' gap="2" className='mt-4'>
+                    <Button size="sm" variant="outline-primary" className='ms-auto'>Add Expense</Button>
+                    <Button size="sm" variant="outline-secondary">View Expense</Button>
+                    <Button size="sm" variant="outline-success">Remove Expense</Button>
+                </Stack>
+            </Card.Body>
+        </Card>
+    )
+}
+
+function getProBarVar(amount, availability) {
+    const ratio = amount / availability
+
+    if (ratio < 0.5) {
+        return "danger"
+    } else if (ratio < 0.75) {
+        return "warning"
+    } else {
+        return "danger"
+    }
+}
+
+export default BudgetCard
