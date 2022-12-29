53617b88f197622b335451167156db7deba29b7f
qnhn22 <nhatquan234@gmail.com>
2022-12-29 00:57:23 -0500

    working on context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BudgetsProvider } from './BudgetsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BudgetsProvider>
+      <App />
+    </BudgetsProvider>
   </React.StrictMode>
 );
 
+// everything in the BudgetContext is going to be available in our app.
