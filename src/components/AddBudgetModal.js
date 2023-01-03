808787fa3cfa5b831a113b496e6d7d4ab32ae42d
qnhn22 <nhatquan234@gmail.com>
2023-01-02 23:22:28 -0500

    working on modals

--- src/components/AddBudgetModal.js
+++ src/components/AddBudgetModal.js
@@ -1,10 +1,28 @@
+import { Form } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function AddBudgetModal() {
+function AddBudgetModal({ show, handleClose }) {
     return (
-        <Modal>
-            <Form></Form>
+        <Modal show={show} onHide={handleClose}>
+            <Form onSubmit={handleSubmit}>
+                <Modal.Header closeButton>
+                    <Modal.Title>New Budget</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className='mb-3' controlId='name'>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type='text' required />
+                    </Form.Group>
+                    <Form.Group className='mb-3' controlId='availability'>
+                        <Form.Label>Availability</Form.Label>
+                        <Form.Control type='number' required min={0} step={0.1} />
+                    </Form.Group>
+                    <div className='d-flex justify-content-end'>
+                        <Button type='submit' color='primary'>Add</Button>
+                    </div>
+                </Modal.Body>
+            </Form>
         </Modal>
     )
 }
--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 
-const BudgetsContext = React.createContext()
+export const BudgetsContext = React.createContext()
 
 export function useBudgets() {
     return useContext(BudgetsContext)
