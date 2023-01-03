f5f2a1d9807c821b178820a0d8d24ee1be3fc8ef
qnhn22 <nhatquan234@gmail.com>
2023-01-02 23:22:05 -0500

    working on modal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import Container from 'react-bootstrap/Container';
 import { Stack, Button } from 'react-bootstrap';
 import BudgetCard from './components/BudgetCard';
+import AddBudgetModal from './components/AddBudgetModal';
 
 function App() {
   return (
-    <Container className='mt-3'>
-      <Stack direction='horizontal' gap={2} className="mb-4">
-        <h1 className='me-auto'>Budgetify</h1>
-        <Button variant='primary'>Add Budget</Button>
-        <Button variant='secondary'>Add Expense</Button>
-      </Stack>
-      <Container fluid>
-        <BudgetCard name={"Housing"} amount={2500} availability={2000} />
-        <BudgetCard name={"Food"} amount={800} availability={1200} />
-        <BudgetCard name={"Entertainment"} amount={500} availability={1000} />
-        <BudgetCard />
+    <>
+      <Container className='mt-3'>
+        <Stack direction='horizontal' gap={2} className="mb-4">
+          <h1 className='me-auto'>Budgetify</h1>
+          <Button variant='primary'>Add Budget</Button>
+          <Button variant='secondary'>Add Expense</Button>
+        </Stack>
+        <Container fluid>
+          <BudgetCard name={"Housing"} amount={2500} availability={2000} />
+          <BudgetCard name={"Food"} amount={800} availability={1200} />
+          <BudgetCard name={"Entertainment"} amount={500} availability={1000} />
+          <BudgetCard />
+        </Container>
+
       </Container>
-    </Container>
+      <AddBudgetModal />
+    </>
   )
 }
 
